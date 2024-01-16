a02b279dfa54dd9c4fe482a34b5bf197a4fd99cf
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-16 17:28:54 +0700

    testing

--- src/consumer.js
+++ src/consumer.js
@@ -1,3 +1,27 @@
+// require('dotenv').config();
+// const ampq = require('amqplib');
+// const NotesService = require('./NotesService');
+// const MailSender = require('./MailSender');
+// const Listener = require('./listener');
+
+// const init = async () => {
+//   const notesService = new NotesService();
+//   const mailSender = new MailSender();
+//   const listener = new Listener(notesService, mailSender);
+
+//   const connection = await ampq.connect(process.env.RABBITMQ_SERVER);
+//   const channel = await connection.createChannel();
+//   await channel.assertQueue('export:notes', {
+//     durable: true,
+//   });
+
+//   await channel.consume('export:notes', listener.listen, {
+//     noAck: true,
+//   });
+// };
+
+// init();
+
 require('dotenv').config();
 const ampq = require('amqplib');
 const NotesService = require('./NotesService');
@@ -5,19 +29,24 @@ const MailSender = require('./MailSender');
 const Listener = require('./listener');
 
 const init = async () => {
-  const notesService = new NotesService();
-  const mailSender = new MailSender();
-  const listener = new Listener(notesService, mailSender);
+  try {
+    const notesService = new NotesService();
+    const mailSender = new MailSender();
+    const listener = new Listener(notesService, mailSender);
 
-  const connection = await ampq.connect(process.env.RABBITMQ_SERVER);
-  const channel = await connection.createChannel();
-  await channel.assertQueue('export:notes', {
-    durable: true,
-  });
+    const connection = await ampq.connect(process.env.RABBITMQ_SERVER, { timeout: 5000 });
+    const channel = await connection.createChannel();
+    await channel.assertQueue('export:notes', {
+      durable: true,
+    });
 
-  await channel.consume('export:notes', listener.listen, {
-    noAck: true,
-  });
+    await channel.consume('export:notes', listener.listen, {
+      noAck: true,
+    });
+  } catch (error) {
+    console.error('Error connecting to RabbitMQ:', error.message);
+    process.exit(1); // Exit the process with an error code
+  }
 };
 
 init();
