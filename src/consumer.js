346adc7f75452ee349655fdaae83149ca0a9765b
Salsabila Fauziah <89504889+salsabilafauzh@users.noreply.github.com>
2024-01-16 16:40:58 +0700

    test connection rabbit amazon MQ

--- src/consumer.js
+++ src/consumer.js
@@ -9,7 +9,13 @@ const init = async () => {
   const mailSender = new MailSender();
   const listener = new Listener(notesService, mailSender);
 
-  const connection = await ampq.connect(process.env.RABBITMQ_SERVER);
+  //   const connection = await ampq.connect(process.env.RABBITMQ_SERVER);
+  const connection = await ampq.connect({
+    hostname: process.env.RABBITMQ_SERVER,
+    port: 5672,
+    username: process.env.RABBITMQ_USER,
+    password: process.env.RABBITMQ_PASSWORD,
+  });
   const channel = await connection.createChannel();
   await channel.assertQueue('export:notes', {
     durable: true,
